6dc0170f5462ebdca23c906a2579db9320394e10
CostaFot <con.fotiadis@gmail.com>
2023-04-01 19:27:48 +0300

    WIP compose template

--- app/src/main/java/com/feelsokman/androidtemplate/domain/JsonPlaceHolderRepository.kt
+++ app/src/main/java/com/feelsokman/androidtemplate/domain/JsonPlaceHolderRepository.kt
@@ -5,8 +5,6 @@ import com.feelsokman.androidtemplate.domain.model.DomainTodo
 import com.feelsokman.androidtemplate.net.JsonPlaceHolderService
 import com.feelsokman.androidtemplate.result.Result
 import com.feelsokman.androidtemplate.result.attempt
-import com.feelsokman.androidtemplate.result.error.ErrorMapper
-import com.feelsokman.androidtemplate.result.error.GenericError
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -14,9 +12,9 @@ class JsonPlaceHolderRepository @Inject constructor(
     private val jsonPlaceHolderService: JsonPlaceHolderService,
     private val dispatcherProvider: DispatcherProvider
 ) {
-    suspend fun getTodo(todoId: Int): Result<GenericError, DomainTodo> =
+    suspend fun getTodo(todoId: Int): Result<Throwable, DomainTodo> =
         withContext(dispatcherProvider.io) {
-            attempt(ErrorMapper::map) {
+            attempt {
                 val apiTodo = jsonPlaceHolderService.getTodo(todoId)
                 DomainTodo(apiTodo.id!!, apiTodo.title!!, apiTodo.completed!!)
             }
