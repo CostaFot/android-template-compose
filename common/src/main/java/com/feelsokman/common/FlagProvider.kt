a2d55a9851cdeb08363bc0a2f04ee9c9ab3df1a2
CostaFot <con.fotiadis@gmail.com>
2023-04-10 23:19:53 +0300

    WIP compose template

--- common/src/main/java/com/feelsokman/common/FlagProvider.kt
+++ common/src/main/java/com/feelsokman/common/FlagProvider.kt
@@ -1,15 +1,9 @@
 package com.feelsokman.common
 
-import com.feelsokman.common.di.DebugFlag
-import com.feelsokman.common.di.RunningUiTestFlag
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class FlagProvider @Inject constructor(
-    @DebugFlag private val debugFlag: Boolean,
-    @RunningUiTestFlag private val uiTestFlag: Boolean
-) {
-    val isDebugEnabled = debugFlag
-    val isRunningUiTest = uiTestFlag
+interface FlagProvider {
+    val isDebugEnabled: Boolean
+    val isRunningUiTest: Boolean
 }
+
+
