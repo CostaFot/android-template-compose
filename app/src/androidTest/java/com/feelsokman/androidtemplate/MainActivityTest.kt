4c1e9ea7b7e4375bc4f5b4efcb071442a3646a28
CostaFot <con.fotiadis@gmail.com>
2023-04-10 20:27:48 +0300

    WIP compose template

--- app/src/androidTest/java/com/feelsokman/androidtemplate/MainActivityTest.kt
+++ app/src/androidTest/java/com/feelsokman/androidtemplate/MainActivityTest.kt
@@ -4,8 +4,10 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import com.feelsokman.androidtemplate.ui.activity.MainActivity
+import dagger.hilt.EntryPoints
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,6 +22,17 @@ class MainActivityTest {
      */
     @get:Rule(order = 1)
     val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setup() {
+        // This is how to reach into SingletonComponent for the test application
+        // prefer TestInstallIn imo
+        val appInitializer = EntryPoints.get(
+            composeTestRule.activity.application,
+            ApplicationEntryPoint::class.java
+        ).appInitializer()
+
+    }
 
     @Test
     fun firstScreen_isForYou() {
--- app/src/main/java/com/feelsokman/androidtemplate/TemplateApplication.kt
+++ app/src/main/java/com/feelsokman/androidtemplate/TemplateApplication.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import androidx.work.Configuration
 import com.feelsokman.androidtemplate.core.initialize.AppInitializer
 import com.feelsokman.logging.logDebug
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -26,4 +29,10 @@ class TemplateApplication : Application(), Configuration.Provider {
         return workerConfiguration
     }
 
+}
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface ApplicationEntryPoint {
+    fun appInitializer(): AppInitializer
 }
