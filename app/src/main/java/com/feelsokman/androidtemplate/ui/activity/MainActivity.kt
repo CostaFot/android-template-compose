a37ab84af39f8d756ca68a2850c90ec44b1b2502
Costa Fotiadis <con.fotiadis@gmail.com>
2026-02-22 11:26:20 +0200

    updates

--- app/src/main/java/com/feelsokman/androidtemplate/ui/activity/MainActivity.kt
+++ app/src/main/java/com/feelsokman/androidtemplate/ui/activity/MainActivity.kt
@@ -130,9 +130,7 @@ class MainActivity : AppCompatActivity() {
                         ),
                         entryProvider = entryProvider {
                             entry<RouteA> {
-                                ContentGreen(goToSecondScreen = {
-                                    backStack.add(RouteB("123"))
-                                })
+                                MainScreen()
                             }
                             entry<RouteB> { key ->
                                 ContentBlue("Route id: ${key.id} ")
